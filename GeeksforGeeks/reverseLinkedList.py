9f3854c0e42adaaa0a7fa4e08ae1702f79f417e0
Jason Lim <31980053+dreamcatcher12@users.noreply.github.com>
2019-10-03 22:34:42 -0700

    Update reverseLinkedList.py

--- GeeksforGeeks/reverseLinkedList.py
+++ GeeksforGeeks/reverseLinkedList.py
@@ -59,23 +59,4 @@ class LinkedList:
             temp = temp.next
   
   
-# Driver program 
-llist = LinkedList() 
-llist.push(8) 
-llist.push(7) 
-llist.push(6) 
-llist.push(5) 
-llist.push(4) 
-llist.push(3) 
-llist.push(2) 
-llist.push(1) 
-  
-print "Given linked list"
-llist.printList() 
-  
-llist.reverse() 
-  
-print "\nReverse linked list"
-llist.printList() 
-  
 # This code is contributed by Nikhil Kumar Singh(nickzuck_007) 
