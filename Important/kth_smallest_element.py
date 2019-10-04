be280ca99577dfd7dc706a40f1c335cf8091ee49
Sarthak Bharadwaj <sarthakbh321@gmail.com>
2019-10-04 13:40:04 +0530

    added info to the script

--- Important/kth_smallest_element.py
+++ Important/kth_smallest_element.py
@@ -1,3 +1,18 @@
+'''
+You are given an array of integers, and you have to find the k-th smallest number in the array.
+
+Input Format:
+
+The first line contains the value of K
+The second line contains a space separated list of integers
+
+
+Output Format:
+
+The only line contains the k-th smallest number in the array
+
+'''
+
 k = int(input("Enter K: "))
 print("Enter the array: ")
 arr = list(map(int, input().split(" ")))
