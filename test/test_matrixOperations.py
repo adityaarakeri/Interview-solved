08703ea37b0cbe4b0746dc82408ae5315b1bdcda
vidya <vidya.vishnusonawane@philips.com>
2019-10-05 14:05:06 +0530

    added unit tests for matrixOperations.py

---
+++ test/test_matrixOperations.py
@@ -0,0 +1,33 @@
+import sys
+#To use the matrixOperations.py in this file, you have to create a new environment variable as 
+# PYTHONPATH = 'path of folder containing matrixOperations.py' 
+import unittest
+import matrixOperations
+
+class testMatrixOperations(unittest.TestCase):
+
+    #Test to check the transpose of given matrix is correct
+    def test_transpose(self):
+        transpose = matrixOperations.transpose([[2, 5, 3], [4, 7, 6],[3, 8, 4]])
+        self.assertTrue(transpose, [[2, 4, 3], [5, 7, 8], [3, 6, 4]])
+   
+    #Test to check the transpose is 3X3 matrix
+    def test_transpose_size(self):
+        transpose = matrixOperations.transpose([[2, 5, 3], [4, 7, 6],[3, 8, 4]])
+        self.assertTrue(len(transpose),3)   # row size
+        self.assertTrue(len(transpose[0]),3)    # column size
+
+    #Test to check the multiplication of given matrix and its transpose is correct
+    def test_mult(self):
+        mult = matrixOperations.mult([[2, 5, 3], [4, 7, 6],[3, 8, 4]],[[2, 4, 3], [5, 7, 8], [3, 6, 4]])
+        self.assertTrue(mult,[[38, 61, 58], [61, 101, 92], [58, 92, 89]] )
+
+    #Test to check the multiplication of two matrices is 3X3 matrix.
+    def test_mult_size(self):
+        mult = matrixOperations.mult([[2, 5, 3], [4, 7, 6],[3, 8, 4]],[[2, 4, 3], [5, 7, 8], [3, 6, 4]])
+        self.assertTrue(len(mult),3)    # row size  
+        self.assertTrue(len(mult[0]),3) # column size
+
+if __name__ == "__main__":
+    unittest.main()
+    
