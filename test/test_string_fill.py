c9564dc5c0f2303e07c324678c413d600d94af16
Dmitry Poliuha <dm.poliuha@gmail.com>
2019-10-11 10:51:11 +0300

    add test

---
+++ test/test_string_fill.py
@@ -0,0 +1,13 @@
+import sys
+import os
+sys.path.append(os.path.join(sys.path[0], '..'))
+from Company.string_fill import string_fill
+
+
+def test_string_fill():
+    assert string_fill("abc", 2) == "abc"
+    assert string_fill("abc", 7) == "    abc"
+
+
+if __name__ == '__main__':
+    test_string_fill()
