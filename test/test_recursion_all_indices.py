5276c3166096a48a47f1bfdf56e9bb047668741e
= <=>
2019-10-14 12:55:09 +0530

    deleted redundant lines

--- test/test_recursion_all_indices.py
+++ test/test_recursion_all_indices.py
@@ -26,6 +26,3 @@ def test_recursion_all_indices():
     assert AllIndexes([34,57,82,41,65,35,82,27,36,12,6,40,66,99,25,29,22,25,12,24,65,15,5,43,28,33,76,32,13,95,22,84,71,23,28,7,65,94,18,47,9,42,61,73],61) ==[42], "Should be 42"
     assert AllIndexes([21,22,46,12,61,25,33,16,99,96,25],25) ==[5,10], "Should be 5 and 10"
 
-if __name__ == "__main__":
-    test_recursion_all_indices()
-    print("Everything passed")
