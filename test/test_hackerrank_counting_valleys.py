1dbe34cc0af0265a09207a42a4fad6e87267de71
Theofanis <theofanis.ch@hotmail.com>
2019-10-17 00:53:30 +0100

    added counting valleys-HackerRank and test

---
+++ test/test_hackerrank_counting_valleys.py
@@ -0,0 +1,28 @@
+from HackerRank.counting_valleys import Solution
+
+"""
+    'D' represents one step down 
+    'U' represents one step up 
+    valley: is a sequence of consecutive steps below sea 
+            level, starting with a step down from sea level and ending 
+            with a step up to sea level
+"""
+
+def test1(sol):
+    steps = 'UDDDUDUU'
+    awns = sol.countingValleys(len(steps), steps)
+    print('steps = '+steps)
+    print("number of valleys = "+str(awns))
+
+
+def test2(sol):
+    steps = 'DDUUDDUDUUUD'
+    awns = sol.countingValleys(len(steps), steps)
+
+    print('steps = '+steps)
+    print("number of valleys = "+str(awns))
+
+
+sol = Solution()
+test1(sol)
+test2(sol)
