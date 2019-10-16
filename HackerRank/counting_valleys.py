e941140fd875f48d21b224cc811e6f462559b5d5
Theofanis <theofanis.ch@hotmail.com>
2019-10-17 00:29:21 +0100

    added problem and test

--- HackerRank/counting_valleys.py
+++ HackerRank/counting_valleys.py
@@ -3,25 +3,34 @@ https://www.hackerrank.com/challenges/counting-valleys/problem?h_l=interview&pla
 
 """
 
+"""
+    'D' represents one step down 
+    'U' represents one step up 
+    valley: is a sequence of consecutive steps below sea 
+            level, starting with a step down from sea level and ending 
+            with a step up to sea level
+            
+"""
 
 # Complete the coutingValleys function below
+class Solution(object):
+    def countingValleys(self,n, s):
+        valleys = 0
+        level = 0
 
-def countingValleys(n, s):
-    valleys = 0
-    level = 0
+        for c in s:
+            if c is 'D':
+                level -= 1
+            elif c is 'U':
+                level += 1
 
-    for c in s:
-        if c is 'D':
-            level -= 1
-        elif c is 'U':
-            level += 1
-
-            if level is 0:
-                valleys += 1
-    return valleys
+                if level is 0:
+                    valleys += 1
+        return valleys
 
 
 if __name__ == '__main__':
+    sol = Solution()
     steps = 'UDDDUDUU'
-    awns = countingValleys(len(steps), steps)
+    awns = sol.countingValleys(len(steps), steps)
     print(awns)
