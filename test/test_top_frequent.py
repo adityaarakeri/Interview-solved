b2e0651fb28bb7686936e08ad0a62051453ad291
Anmollenka <anmollenka234@gmail.com>
2020-09-26 14:14:36 +0530

    Required Changes

--- test/test_top_frequent.py
+++ test/test_top_frequent.py
@@ -2,7 +2,7 @@ import sys
 import os
 sys.path.append(os.path.join(sys.path[0], '..'))
 from LeetCode.top_k_frequent import Solution
- 
+
 def test_topk():
     s = Solution()
     assert s.topKFrequent([1,1,1,2,2,3],2) == [1,2]
