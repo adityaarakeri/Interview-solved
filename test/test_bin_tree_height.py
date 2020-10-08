8c2c12c848318ddba75db0f10720dc5b2f42f706
Ron Urbach <ruurbach@amazon.com>
2020-10-08 10:04:16 +0300

    changed repo structure for TreesAndGraphs dir

--- test/test_bin_tree_height.py
+++ test/test_bin_tree_height.py
@@ -1,6 +1,6 @@
 import sys
 import os
-sys.path.append(os.path.join(sys.path[0], '../Trees and Graphs/'))
+sys.path.append(os.path.join(sys.path[0], '../TreesAndGraphs/'))
 from bin_tree_height import unit_test
 
 def test_bin_tree_height():
