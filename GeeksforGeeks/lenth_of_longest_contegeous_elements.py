c4180e71b17a01b8fe3ffb7cfe808f9ad3041f4c
Parnabrita Mondal <72441280+hitblunders@users.noreply.github.com>
2020-10-13 09:48:32 -0400

    Update lenth_of_longest_contegeous_elements.py

--- GeeksforGeeks/lenth_of_longest_contegeous_elements.py
+++ GeeksforGeeks/lenth_of_longest_contegeous_elements.py
@@ -14,28 +14,34 @@ Output: Length of the longest contiguous subarray is 2
 Input:  arr[] = {1, 56, 58, 57, 90, 92, 94, 93, 91, 45};
 Output: Length of the longest contiguous subarray is 5
 """
-def min(x, y): 
-    return x if(x < y) else y 
-      
-def max(x, y): 
-    return x if(x > y) else y 
 
 def find_length(arr):
-    n = len(arr)
-    long_len = 1
-    start = 0
-    while start<(n-1):
-     
-        mnm = mxm = arr[start]
-        for end in range(start+1, n):
-            mnm = min(mnm,arr[end])
-            mxm = max(mxm, arr[end])
-            if mxm-mnm == end-start:
-                long_len = max(long_len, mxm-mnm+1)
-        start += 1
-    return long_len
 
-A = [1, 56, 58, 57, 90, 92, 94, 93, 91, 45]
+    arr.sort()
+    n = len(arr)
+
+    long_len = 1
+    result = []
+
+    start = 0
+
+    while start < n-1:
+
+        if arr[start] == arr[start+1] - 1:
+            long_len += 1
+        else:
+            result.append(long_len)
+            long_len = 1
+        start += 1
+    
+    result.append(long_len)
+
+    if max(result) != 1:
+        return max(result)
+    else:
+        return -1
+
+# A = [1, 56, 58, 57, 90, 92, 94, 93, 91, 45]
 # A = [10, 12, 11]
-# A = [15, 13, 11, 20]
-print(find_length(A))
+A = [15, 13, 11, 20]
+print(find_length(A)))
